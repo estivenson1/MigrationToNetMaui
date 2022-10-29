eb2ea6f280f9f9690134d54baf0c791409e6e66f
Estivenson1 <Estivenson1@hotmail.com>
2022-10-29 04:42:50 -0500

    Creación y actualización del proyecto Migration.Framework.Manual.App

---
+++ Migration.Framework.Manual.App/Migration.Framework.Manual.App/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Migration.Framework.Manual.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
